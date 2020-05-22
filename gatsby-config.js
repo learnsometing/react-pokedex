88c521e407434b8628a6ab3ebf1dd59d7c7ba733
Brian Monaccio <brianmonaccio@gmail.com>
2020-05-22 13:16:19 -0400

    Removes unneeded file system source to prevent deploy failure

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `cries`,
         path: `${__dirname}/src/data/cries`,
       },
